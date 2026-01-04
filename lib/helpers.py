d0fc577449cc3b9e5461b39ac785ed9fdd455df7
bururun <bururun@users.noreply.github.com>
2026-01-04 12:01:29 +0200

    remove: fix race condition

--- lib/helpers.py
+++ lib/helpers.py
@@ -51,3 +51,14 @@ def helper_function_18(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_23(x):
+    """Helper function for iteration 23."""
+    return x * 23
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
